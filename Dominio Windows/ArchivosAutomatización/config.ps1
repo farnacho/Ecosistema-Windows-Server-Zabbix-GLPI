1daa15f0311b19ff0d3ce9974ce97676a7ae1159
farnacho <abadrodrigueznacho@gmail.com>
2026-03-30 11:09:54 +0200

    Delete Dominio Windows directory

--- Dominio Windows/ArchivosAutomatización/config.ps1
+++
@@ -1,8 +0,0 @@
-$global:DB_Server = "localhost"
-$global:DB_Port   = 3306
-$global:DB_Name   = "gestion_usuarios"
-$global:DB_User   = "root"
-$global:DB_Pass   = "Server22."
-
-$global:AD_Domain = "nacho.local"
-$global:AD_Server = "server"
